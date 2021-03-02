d4af2b6a0ec9a3dbf9284a11abd9867fac4dbcb9
Fevers <47673608+FortniteFevers@users.noreply.github.com>
2021-03-02 14:33:18 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ FileFormat = '.ucas'
 
 #---------------#
 
-
+print('')
 print('----------------------------')
 print(Fore.GREEN + f'Loaded file path as {FortniteGamePath}')
 print(f'\nLoaded FileFormat as {FileFormat}')
@@ -22,6 +22,8 @@ print(Fore.WHITE + '----------------------------')
 thing = FileFormat.replace('.', '')
 print(Fore.GREEN + f'\nGrabbing current {thing} files...')
 time.sleep(0.5)
+
+counter = 1
 
 try:
     for file in os.listdir(FortniteGamePath): # For every single file in the paks folder, do this
@@ -34,7 +36,9 @@ try:
                 lol = lol/1000 # Converts the kb to mb
                 lol = round(lol, 1) # Rounds it up so theres no ugly decimals
                 file = file.replace('.ucas', '')
-                print(f'\n'+Fore.GREEN+f'{file} - '+Fore.YELLOW+f'{lol}mb') # Prints out file information
+                uh = len(FortniteGamePath)
+                print(f'\n'+Fore.GREEN+f'{file} - '+Fore.YELLOW+f'{lol}mb')
+                counter = counter + 1
                 time.sleep(0.05)
         except:
             print (Fore.RED+'\nAn error has been found. You most likely have put in the wrong file path, try again!')
@@ -45,6 +49,7 @@ except:
     time.sleep(3)
     exit()
 
-
+x = counter-1
+print(f'\n\nFound {x} "{thing}" files.')
 print(Fore.CYAN + '\nSuccesfully Grabbed File Sizes!' + Fore.RESET)
 time.sleep(500)
